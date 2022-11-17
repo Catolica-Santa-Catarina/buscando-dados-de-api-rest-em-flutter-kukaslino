4410dc4dacb282479c8d9b93315681a74714553d
CAUE GIRARDI PETRI <caue.petri@catolicasc.edu.br>
2022-11-16 21:32:32 -0300

    corrigindo merge

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:tempo_template/services/location.dart';
 import 'package:http/http.dart' as http;
-=======
->>>>>>> 4d15b131abd95141f794c7f7983a1c3e0ede6809
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -13,7 +10,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-<<<<<<< HEAD
 
   Future<void> getLocation() async {
     Location location = Location();
@@ -44,10 +40,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     getData();
-=======
-  @override
-  Widget build(BuildContext context) {
->>>>>>> 4d15b131abd95141f794c7f7983a1c3e0ede6809
     return Scaffold(
       body: Center(
         child: ElevatedButton(
